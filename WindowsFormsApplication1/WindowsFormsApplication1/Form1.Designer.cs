bec92e0055933d183f2c14a33f61f9f9bbcf8b83
dev-idebeda <wow@idebeda.com>
2014-12-20 12:29:38 +0700

    source code program monitoring % baterai laptop
    auto on ketika baterai < 30%
    auto off setelah baterai penuh

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BatteryIndicator = new System.Windows.Forms.ProgressBar();
+            this.lbl_charg = new System.Windows.Forms.Label();
+            this.ChargeRemaining = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblBatteryName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BatteryIndicator
+            // 
+            this.BatteryIndicator.Location = new System.Drawing.Point(6, 19);
+            this.BatteryIndicator.Name = "BatteryIndicator";
+            this.BatteryIndicator.Size = new System.Drawing.Size(286, 32);
+            this.BatteryIndicator.TabIndex = 0;
+            // 
+            // lbl_charg
+            // 
+            this.lbl_charg.AutoSize = true;
+            this.lbl_charg.Location = new System.Drawing.Point(6, 22);
+            this.lbl_charg.Name = "lbl_charg";
+            this.lbl_charg.Size = new System.Drawing.Size(95, 13);
+            this.lbl_charg.TabIndex = 1;
+            this.lbl_charg.Text = "Charge remaining :";
+            // 
+            // ChargeRemaining
+            // 
+            this.ChargeRemaining.AutoSize = true;
+            this.ChargeRemaining.Location = new System.Drawing.Point(102, 22);
+            this.ChargeRemaining.Name = "ChargeRemaining";
+            this.ChargeRemaining.Size = new System.Drawing.Size(13, 13);
+            this.ChargeRemaining.TabIndex = 3;
+            this.ChargeRemaining.Text = "?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BatteryIndicator);
+            this.groupBox1.Location = new System.Drawing.Point(35, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 60);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Monitor";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_charg);
+            this.groupBox2.Controls.Add(this.ChargeRemaining);
+            this.groupBox2.Location = new System.Drawing.Point(35, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 44);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(119, 66);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(134, 72);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Port";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(39, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblBatteryName
+            // 
+            this.lblBatteryName.AutoSize = true;
+            this.lblBatteryName.Location = new System.Drawing.Point(112, 16);
+            this.lblBatteryName.Name = "lblBatteryName";
+            this.lblBatteryName.Size = new System.Drawing.Size(35, 13);
+            this.lblBatteryName.TabIndex = 11;
+            this.lblBatteryName.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Battery Name :";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(381, 218);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblBatteryName);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(373, 192);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Battery Auto Charging";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(373, 192);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Manual Control";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.linkLabel1);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(373, 192);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "About";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(99, 146);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(171, 13);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Tag = "";
+            this.linkLabel1.Text = "By: idebeda.com | Cirebon @ 2014";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(291, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "contoh dan rangsangan bagi yang lain untuk turut berkreasi.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(319, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "idebeda Piranti Cerdas meng-open source-kan program ini sebagai";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(290, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kamupun bisa membuat program yang memanfaatkan Volta.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(325, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ini adalah software/program yang dibuat tim idebeda Piranti Cerdas ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(346, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "untuk produk Volta (terminal listrik cerdas). Dibuat dengan C# VS 2010. ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Charger off bila baterai sudah 98% | on bila < 30%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 245);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "idebeda Volta";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar BatteryIndicator;
+        private System.Windows.Forms.Label lbl_charg;
+        private System.Windows.Forms.Label ChargeRemaining;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblBatteryName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+        string  a = "awal ngecas";
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        
+        PowerStatus status = SystemInformation.PowerStatus;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery where Name='" + lblBatteryName.Text + "'");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                PowerStatus status = SystemInformation.PowerStatus;
+                BatteryIndicator.Value = (int)(status.BatteryLifePercent * 100);
+                ChargeRemaining.Text = String.Format("{0}%", (status.BatteryLifePercent * 100));
+
+                float b = status.BatteryLifePercent*100;
+                //MessageBox.Show(a);
+
+                if (a == "awal ngecas")
+                {
+
+                    if (b <= 98)
+                    {
+                        Process firstProc = new Process();
+                        firstProc.StartInfo.FileName = "onae commandline.exe";
+                        firstProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        firstProc.StartInfo.Arguments = "on 0";
+                        firstProc.EnableRaisingEvents = true;
+                        firstProc.Start();
+
+                        if (b == 98) {
+
+                            a = "selesai ngecas";
+                        }
+                    }
+                    
+                }
+                else if(a == "selesai ngecas"){
+                    Process firstProc = new Process();
+                    firstProc.StartInfo.FileName = "onae commandline.exe";
+                    firstProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    firstProc.StartInfo.Arguments = "off 0";
+                    firstProc.EnableRaisingEvents = true;
+                    firstProc.Start();
+
+                    if (b > 30)
+                    {
+
+
+                    }
+                    else {
+
+                        a = "awal ngecas";
+                    }
+                                    
+                }
+                
+            }
+                   
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                lblBatteryName.Text = mo["Name"].ToString();
+            }
+
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            //switch (MessageBox.Show(this, "Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo))
+            //{
+            //   case DialogResult.No:
+            //        e.Cancel = true;
+            //        break;
+            //    default:
+            //         break;
+            // }
+            Process firstProc = new Process();
+            firstProc.StartInfo.FileName = "onae commandline.exe";
+            firstProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            firstProc.StartInfo.Arguments = "off 0";
+            firstProc.EnableRaisingEvents = true;
+
+            firstProc.Start();
+            //firstProc.WaitForExit();
+
+            Process secondProc = new Process();
+            secondProc.StartInfo.FileName = "onae commandline.exe";
+            secondProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            secondProc.StartInfo.Arguments = "off 1";
+            secondProc.EnableRaisingEvents = true;
+
+            secondProc.Start();
+            //secondProc.WaitForExit();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process firstProc = new Process();
+            firstProc.StartInfo.FileName = "onae commandline.exe";
+            firstProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            firstProc.StartInfo.Arguments = "on " + comboBox1.SelectedIndex;
+            firstProc.EnableRaisingEvents = true;
+
+            firstProc.Start();
+            //firstProc.WaitForExit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process firstProc = new Process();
+            firstProc.StartInfo.FileName = "onae commandline.exe";
+            firstProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            firstProc.StartInfo.Arguments = "off " + comboBox1.SelectedIndex;
+            firstProc.EnableRaisingEvents = true;
+
+            firstProc.Start();
+            //firstProc.WaitForExit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited. 
+            this.linkLabel1.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("http://idebeda.com");
+        }
+
+        
+    }
+}
